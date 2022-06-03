93d5ad0c8a2926f616834e7043800d279028fc13
Josh Rondestvedt <atticus183@gmail.com>
2022-06-03 17:15:56 -0500

    Initial commit

---
+++ Checkd/CheckdApp.swift
@@ -0,0 +1,20 @@
+//
+//  CheckdApp.swift
+//  Checkd
+//
+//  Created by Josh R on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckdApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
