a41e19198bf3c2d707185a0e97864b6b3fa9e43b
Josh Rondestvedt <atticus183@gmail.com>
2022-06-03 17:25:09 -0500

    Creates initial project structure

--- Checkd/CheckdApp.swift
+++ Checkd/CheckdApp.swift
@@ -13,7 +13,7 @@ struct CheckdApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
--- Checkd/Views/ListView/ListView.swift
+++ Checkd/Views/ListView/ListView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct ListView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
@@ -83,6 +83,6 @@ private let itemFormatter: DateFormatter = {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ListView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
